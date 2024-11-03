28815c8d7f5cdd487b5a4bf7a60188055ba0071a
Velihan Gözek <velihangozek@hotmail.com>
2024-11-04 00:15:55 +0300

    update 08-10-2024.sql delete comment indicators

--- 08-10-2024.sql
+++ 08-10-2024.sql
@@ -1,57 +1,57 @@
-/*create database Education
+create database Education
 block comment(*//*) */
 -- use Education --> line comment (--)
 
-/*
+
 create table Egitmenler (
 	EgitmenID int IDENTITY(1,1) PRIMARY KEY,
 	EgitmenAdi nvarchar(100),
 	EgitmenSoyadi nvarchar(100),
 )
-*/
 
-/*
+
+
 create table Egitimler (
 	EgitimID int identity(100,1) PRIMARY KEY,
 	EgitimAdi nvarchar(100) NOT NULL,
 	EgitimTarihi DATE,
 )
-*/
 
-/*
+
+
 create table Ogrenciler (
 	OgrenciID int IDENTITY(1,1) PRIMARY KEY,
 	OgrenciAdi nvarchar(100),
 	OgrenciSoyadi nvarchar(100),
 )
-*/
 
-/*
+
+
 alter table Ogrenciler
 add OgrenciTelefonNumarasi nvarchar(15)
-*/
 
-/*
+
+
 drop table Egitmenler
-*/
 
-/*
+
+
 truncate table Ogrenciler where OgrenciID = 2
-*/
 
-/*
+
+
 exec sp_rename 'Egitimler', 'Bootcampler'
-*/
 
-/*
+
+
 create table Egitmenler(
 	EgitmenID int IDENTITY(1,1) PRIMARY KEY,
 	EgitmenAdi nvarchar(100),
 	EgitmenSoyadi nvarchar(100),
 )
-*/
 
-/*
+
+
 select * from egitmenler
 
 select EgitmenID, EgitmenSoyadi from egitmenler
@@ -61,4 +61,3 @@ select EgitmenAdi as Ad from egitmenler
 select EgitimAdi from Bootcampler
 
 select EgitimTarihi as BootcampDersTarihi from Bootcampler
-*/
